4337b9531394febfd32fbce6c1fc88eacfefe18f
brothermagroticus <nigratusmaxima@gmail.com>
2019-11-24 11:33:50 -0400

    PracticaIII

---
+++ p3e4.py
@@ -0,0 +1,13 @@
+v1 = int(input('Inserte el primer numero: '))
+v2 = int(input('Inserte el segundo numero: '))
+while True:
+    if v1 == v2:
+        print("Los numeros son iguales")
+        break
+    else:
+        print('Los numeros no son iguales')
+        break
+
+
+    
+    
