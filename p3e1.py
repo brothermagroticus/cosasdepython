7a2f5701ae5073e912a5031c0835d07c3f489278
brothermagroticus <57784663+brothermagroticus@users.noreply.github.com>
2019-11-24 02:18:56 -0400

    Delete p3e1.py

--- p3e1.py
+++
@@ -1,6 +0,0 @@
-while True:
-    valorx = float(input('Ingrese la base: '))
-    valory = float(input('Ingrese la potencia: '))
-    resultado = valorx ** valory
-    print(f'El resultado es: {resultado}')
-    break
