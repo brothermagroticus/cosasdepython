9567104953fdbe08a17c90031c1063d1525e7056
brothermagroticus <nigratusmaxima@gmail.com>
2019-11-24 01:13:38 -0400

    PracticaII

---
+++ p3e1.py
@@ -0,0 +1,6 @@
+while True:
+    valorx = float(input('Ingrese la base: '))
+    valory = float(input('Ingrese la potencia: '))
+    resultado = valorx ** valory
+    print(f'El resultado es: {resultado}')
+    break
